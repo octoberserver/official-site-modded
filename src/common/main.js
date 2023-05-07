62e1b6ac7c9cf6d25a8f90f636065c09bd376cf5
october <octoberserver@gmail.com>
2023-05-07 20:42:19 +0800

    added how to join modal

--- src/common/main.js
+++ src/common/main.js
@@ -1,5 +1,5 @@
 {
-    let logoImage = document.getElementsByClassName("nav-logo-img")[0]
+    let logoImage = document.querySelector("[nav-logo-img]");
     let a = logoImage.parentElement;
     a.addEventListener("mouseover", e => logoImage.src = "common/logo-blink-2.gif");
     a.addEventListener("mouseout", e => logoImage.src = "common/logo.png");
