8bb4a0ba38cdbdbd5cd75de7c9e621e83ad1b428
october <octoberserver@gmail.com>
2023-05-08 21:17:02 +0800

    another commit

--- src/common/main.js
+++ src/common/main.js
@@ -1,6 +1,6 @@
 {
     let logoImage = document.querySelector("[nav-logo-img]");
     let a = logoImage.parentElement;
-    a.addEventListener("mouseover", e => logoImage.src = "common/logo-blink-2.gif");
-    a.addEventListener("mouseout", e => logoImage.src = "common/logo.png");
+    a.addEventListener("mouseover", e => logoImage.src = "common/images/icons/logo-blink-2.gif");
+    a.addEventListener("mouseout", e => logoImage.src = "common/images/icons/logo.png");
 }
