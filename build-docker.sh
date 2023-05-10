3bf3b164fef463a02ac31faafffacf265deeb3e2
october <octoberserver@gmail.com>
2023-05-11 00:04:44 +0800

    automated deployment process

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 cd "${0%/*}"
 cd "dist"
+
+read -p 'Deploy to remote?: ' remote
+if [ "$remote" = "y" ]; then
+    docker context use remote
+else
+    docker context use default
+fi
+
+docker build -t octmodsrv/official-site:latest .
+docker run -p80:80 -p443:443 octmodsrv/official-site:latest
+
+if [ "$remote" = "y" ]; then
+    docker context use default
+    echo "reseted docker context to default"
+else
+    docker context use remote
+    echo "reseted docker context to remote"
+fi
--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 cd "${0%/*}"
 cp -r ./src/ ./dist/src/
 ./tmpl-preprocessor/tmpl-pre
+./build-docker.sh
